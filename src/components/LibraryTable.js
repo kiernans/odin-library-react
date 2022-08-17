7d8c455af5d45364c9c400e1c8940c1521f0f177
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-16 21:27:40 -0700

    Create function to handle submit and display to page

--- src/components/LibraryTable.js
+++ src/components/LibraryTable.js
@@ -1,5 +1,11 @@
+import React from "react";
+import { useLibrary } from "./LibraryProvider";
+
 const LibraryTable = () => {
-    return (
+
+    const library = useLibrary();
+
+     return (
         <>
             <table>
                 <thead>
@@ -8,11 +14,20 @@ const LibraryTable = () => {
                         <th>Author</th>
                         <th>Pages</th>
                         <th>Status</th>
-                        <th colspan="1">&nbsp;</th>
+                        <th colSpan="1">&nbsp;</th>
                     </tr>
                 </thead>
                 <tbody>
-
+                    {
+                        library.map(book => {
+                            return <tr key={book.id}>
+                                <td>{book.title}</td>
+                                <td>{book.author}</td>
+                                <td>{book.pages}</td>
+                                <td>{book.status}</td>
+                            </tr>
+                        })
+                    }
                 </tbody>
             </table>
         </>
